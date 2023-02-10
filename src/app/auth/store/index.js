b32ff76b896d0b6869d8868c2d6c744b57630bfc
Asylbek Ibrakhimov <112480502+asyke@users.noreply.github.com>
2023-02-09 18:38:09 -0600

    style

--- src/app/auth/store/index.js
+++ src/app/auth/store/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from '@reduxjs/toolkit';
 import login from './loginSlice';
 import register from './registerSlice';
 import user from './userSlice';
-import reset from './resetSlice'
+import reset from './resetSlice';
 
 const authReducers = combineReducers({
   user,
